3450550684db8a9d15e3470af755f14266c4de22
Fang Wei Hsu <davidhsu1115@gmail.com>
2018-07-27 01:23:33 +0800

    Initial Commit

---
+++ Time Ticker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Time Ticker
+//
+//  Created by fangwiehsu on 2018/7/27.
+//  Copyright © 2018年 fangwiehsu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
